539933414a3939a72cf1ea7baa6664557b2f4e4a
Olin Kirkland <me@olinkirk.land>
2022-05-29 17:57:38 +0200

    Link popups to home screen

--- src/components/platform/HomePage.tsx
+++ src/components/platform/HomePage.tsx
@@ -1,12 +1,48 @@
+import { useEffect, useState } from 'react';
+import Connection, { ConnectionEventType } from '../../controllers/connection';
 import PopupMediator from '../../controllers/popupMediator';
+import { me } from '../../data/user';
+import { PopupInputVerifyCode } from '../popups/PopupInputVerifyCode';
+import { PopupMe } from '../popups/PopupMe';
+import { PopupRegister } from '../popups/PopupRegister';
 import { PopupShop } from '../popups/PopupShop';
 import HomePanel from './HomePanel';
 
 export default function HomePage() {
+  const [isRegistered, setIsRegistered] = useState(me.isRegistered);
+  const [isVerified, setIsVerified] = useState(me.isVerified);
+
+  useEffect(() => {
+    Connection.instance.addListener(
+      ConnectionEventType.USER_DATA_CHANGED,
+      onUserDataChanged
+    );
+
+    return () => {
+      Connection.instance.removeListener(
+        ConnectionEventType.USER_DATA_CHANGED,
+        onUserDataChanged
+      );
+    };
+  }, []);
+
+  function onUserDataChanged() {
+    setIsRegistered(me.isRegistered);
+    setIsVerified(me.isVerified);
+  }
+
   return (
     <div className="page home">
       <div className="home-container">
         <div className="home-grid">
+          <HomePanel
+            onClick={() => {
+              // Connection.instance.joinRandomGame();
+            }}
+            titleText="Join a Random Match"
+            buttonText="Quick Play"
+            image="assets/images/abstract-1.png"
+          />
           <HomePanel
             onClick={() => {
               PopupMediator.open(PopupShop);
@@ -17,29 +53,42 @@ export default function HomePage() {
           />
           <HomePanel
             onClick={() => {
-              // Connection.instance.joinRandomGame();
-            }}
-            titleText="Play Now &amp; Join a Random Match"
-            buttonText="Play Now"
-            image="assets/images/abstract-1.png"
-            big={true}
-          />
-          <HomePanel
-            onClick={() => {
               // PopupMediator.open(PopupHostGame);
             }}
-            titleText="Play with friends"
-            buttonText="Host a game"
+            titleText="Host or Join a Custom Game"
+            buttonText="Custom Game"
             image="assets/images/abstract-4.png"
           />
-          <HomePanel
-            onClick={() => {
-              // PopupMediator.open(PopupJoinGame);
-            }}
-            titleText="View open games"
-            buttonText="Join a game"
-            image="assets/images/abstract-5.png"
-          />
+          {isRegistered && isVerified && (
+            <HomePanel
+              onClick={() => {
+                PopupMediator.open(PopupMe);
+              }}
+              titleText="Customize My Profile"
+              buttonText="My Profile"
+              image="assets/images/abstract-5.png"
+            />
+          )}
+          {isRegistered && !isVerified && (
+            <HomePanel
+              onClick={() => {
+                PopupMediator.open(PopupInputVerifyCode);
+              }}
+              titleText="Get a FREE Gift!"
+              buttonText="Verify Email"
+              image="assets/images/verify-gift.png"
+            />
+          )}
+          {!isRegistered && !isVerified && (
+            <HomePanel
+              onClick={() => {
+                PopupMediator.open(PopupRegister);
+              }}
+              titleText="Register with an Email"
+              buttonText="Sign Up"
+              image="assets/images/abstract-5.png"
+            />
+          )}
         </div>
       </div>
     </div>
